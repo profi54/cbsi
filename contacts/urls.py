78a487f852b8331f04c82dec05d0b653847e3566
alexfedorov81-cloud <alex.fedorov81@gmail.com>
2025-12-14 17:54:06 +0700

    правим url

--- contacts/urls.py
+++ contacts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('callback/', views.callback_request, name='callback'),
-    # Убираем callback_success, так как он больше не нужен с AJAX
+    path('callback/', views.callback_request, name='callback_request'),
+
 ]
