3c25fda91d3a9d724700f58fbe1e09ff72b66b89
alexfedorov81-cloud <alex.fedorov81@gmail.com>
2025-12-14 16:57:52 +0700

    Правки для отображений

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import SiteSettings, SiteStatistics, PrivacyPolicy, ResearchInfo
+from .models import SiteSettings, PrivacyPolicy, ResearchInfo
 
 
 @admin.register(SiteSettings)
