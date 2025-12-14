e0fae7c4ae126aaced3939c621a0342d0e7c755a
alexfedorov81-cloud <alex.fedorov81@gmail.com>
2025-12-14 15:52:47 +0700

    Блок Об исследовании

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import SiteSettings, SiteStatistics, PrivacyPolicy
+from .models import SiteSettings, SiteStatistics, PrivacyPolicy, ResearchInfo
 
 
 @admin.register(SiteSettings)
@@ -13,14 +13,11 @@ class SiteSettingsAdmin(admin.ModelAdmin):
 @admin.register(SiteStatistics)
 class SiteStatisticsAdmin(admin.ModelAdmin):
     list_display = ['tests_completed', 'corporate_clients', 'years_experience', 'updated_at']
-    list_display_links = ['updated_at']  # Добавляем эту строку
+    list_display_links = ['updated_at']
     list_editable = ['tests_completed', 'corporate_clients', 'years_experience']
 
     def has_add_permission(self, request):
         return not SiteStatistics.objects.exists()
-
-
-
 
 
 @admin.register(PrivacyPolicy)
@@ -30,3 +27,48 @@ class PrivacyPolicyAdmin(admin.ModelAdmin):
 
     def has_add_permission(self, request):
         return not PrivacyPolicy.objects.exists()
+
+
+@admin.register(ResearchInfo)
+class ResearchInfoAdmin(admin.ModelAdmin):
+    list_display = ['title', 'is_active', 'order', 'updated_at']
+    list_editable = ['is_active', 'order']
+    list_filter = ['is_active']
+    search_fields = ['title', 'column_1', 'column_2', 'column_3']
+
+    # Поля для формы редактирования
+    fieldsets = (
+        ('Основное', {
+            'fields': ('title', 'is_active', 'order'),
+        }),
+        ('Содержание колонок', {
+            'fields': ('column_1', 'column_2', 'column_3'),
+            'description': 'В каждую колонку можно вписать текст. Используйте HTML для форматирования:<br>'
+                           '• <strong>&lt;strong&gt;жирный текст&lt;/strong&gt;</strong><br>'
+                           '• <em>&lt;em&gt;курсив&lt;/em&gt;</em><br>'
+                           '• &lt;ul&gt;&lt;li&gt;пункт списка&lt;/li&gt;&lt;/ul&gt;<br>'
+                           '• &lt;h4&gt;заголовок&lt;/h4&gt; (небольшой)<br>'
+                           '• &lt;br&gt; - перенос строки<br><br>'
+                           '<strong>Пример для колонки 1:</strong><br>'
+                           '&lt;h4&gt;Точность&lt;/h4&gt;<br>'
+                           'Используем современное оборудование<br>'
+                           '&lt;strong&gt;Диана-7&lt;/strong&gt;'
+        }),
+    )
+
+    # Настраиваем поля для удобства
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+
+        # Для каждой колонки настраиваем внешний вид
+        for field_name in ['column_1', 'column_2', 'column_3']:
+            form.base_fields[field_name].widget.attrs.update({
+                'rows': '10',
+                'style': 'width: 100%; font-family: monospace;',
+                'placeholder': f'Введите текст для {field_name}...\nМожно использовать HTML теги'
+            })
+
+        return form
+
+    # Сортировка по умолчанию
+    ordering = ['order', '-updated_at']
--- core/models.py
+++ core/models.py
@@ -43,3 +43,66 @@ class PrivacyPolicy(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class ResearchInfo(models.Model):
+    """Блок 'Об исследованиях' """
+
+    title = models.CharField(
+        'Заголовок блока',
+        max_length=200,
+        default='О наших исследованиях',
+        help_text='Заголовок блока, например: "Почему выбирают нас"'
+    )
+
+    # Три колонки
+    column_1 = models.TextField(
+        'Колонка 1 (левая)',
+        blank=True,
+        help_text='Текст для первой колонки. Можно использовать HTML: '
+                  '&lt;strong&gt;жирный текст&lt;/strong&gt;, '
+                  '&lt;br&gt; - перенос строки, '
+                  '&lt;ul&gt;&lt;li&gt;список&lt;/li&gt;&lt;/ul&gt;'
+    )
+
+    column_2 = models.TextField(
+        'Колонка 2 (средняя)',
+        blank=True,
+        help_text='Текст для второй колонки. Можно использовать HTML: '
+                  '&lt;strong&gt;жирный текст&lt;/strong&gt;, '
+                  '&lt;br&gt; - перенос строки, '
+                  '&lt;ul&gt;&lt;li&gt;список&lt;/li&gt;&lt;/ul&gt;'
+    )
+
+    column_3 = models.TextField(
+        'Колонка 3 (правая)',
+        blank=True,
+        help_text='Текст для третьей колонки. Можно использовать HTML: '
+                  '&lt;strong&gt;жирный текст&lt;/strong&gt;, '
+                  '&lt;br&gt; - перенос строки, '
+                  '&lt;ul&gt;&lt;li&gt;список&lt;/li&gt;&lt;/ul&gt;'
+    )
+
+    is_active = models.BooleanField(
+        'Показывать на сайте',
+        default=True,
+        help_text='Отметьте, чтобы блок отображался на главной странице'
+    )
+
+    order = models.IntegerField(
+        'Порядок отображения',
+        default=0,
+        help_text='Чем меньше число, тем выше блок. Используется при наличии нескольких блоков'
+    )
+
+    created_at = models.DateTimeField('Дата создания', auto_now_add=True)
+    updated_at = models.DateTimeField('Дата обновления', auto_now=True)
+
+    class Meta:
+        verbose_name = 'Блок "Об исследованиях" (3 колонки)'
+        verbose_name_plural = 'Блоки "Об исследованиях" (3 колонки)'
+        ordering = ['order', '-created_at']
+
+    def __str__(self):
+        status = "активен" if self.is_active else "скрыт"
+        return f"{self.title} ({status})"
